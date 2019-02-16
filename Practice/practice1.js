c24152cbf2be6ccc87cbf7b9874f4459ecf65eea
marjan91 <marjan20.gew16@gmail.com>
2019-02-16 22:52:21 +0600

    practice 1

--- Practice/practice1.js
+++ Practice/practice1.js
@@ -2,6 +2,39 @@ function myFunction(name) {
     return 'Hello, ' + name;
 }
 
-var hello = myFunction('Marjan Khan');
+var hello = myFunction;
+// var myName = myFunction('Marjan Khan');
 
-console.log(hello);
+// console.log(myName);
+
+var anotherHello = hello;
+
+console.log(anotherHello('Marjan Khan'));
+// A function can be stored on a variable.
+ 
+var arr = [1, 2, 3];
+arr.push(anotherHello)
+ console.log(arr);
+// a function can be stored in an arrey.
+
+var persion = {
+    name: 'Marjan Khan',
+    age: 18,
+}
+persion
+ // A function can be stored in an object.
+
+var sum = 10 + (function() {return 50}) ();
+
+sum
+// We can creat function as needed.
+
+function wow(name, fun) {
+    return fun(name);
+}
+
+var result = wow('Marjan Khan', myFunction);
+
+result
+// A function can be pass as a argument.
+
